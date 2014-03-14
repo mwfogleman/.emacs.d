eb7597a571f9ff1410d9d7f8c4a5db6d9a5a9e73
Michael Fogleman <michaelwfogleman@gmail.com>
2014-03-14 19:42:47 +0530

    Moved custom settings to custom.el. Hat tip, Magnars.

---
+++ custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#262626" "#d70000" "#5f8700" "#af8700" "#0087ff" "#af005f" "#00afaf" "#626262"])
+ '(auth-source-save-behavior nil)
+ '(background-color nil)
+ '(background-mode dark)
+ '(cursor-color nil)
+ '(custom-enabled-themes (quote (whiteboard)))
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
+ '(foreground-color nil)
+ '(org-agenda-files (quote ("~/org/todo.org" "~/Dropbox/writing/lim.org" "~/Dropbox/org/notes.org" "~/org/me.org"))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
