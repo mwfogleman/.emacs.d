ad3005c1b6562bd84b926ef8ed4da67cf4e40e55
Michael Fogleman <michaelwfogleman@gmail.com>
2014-09-23 08:57:25 -0400

    Emacs: Custom.el updates.

--- custom.el
+++ custom.el
@@ -9,7 +9,7 @@
  '(background-mode dark)
  '(cursor-color nil)
  '(custom-enabled-themes (quote (solarized-dark)))
- '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
+ '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
  '(foreground-color nil))
 
 (custom-set-faces
