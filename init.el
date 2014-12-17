381780ad12e28a85e796500de5bad958287a4c9d
Michael Fogleman <michaelwfogleman@gmail.com>
2014-12-17 18:14:39 -0500

    Emacs: Use a git version of Org-mode.
    This fixes a bug I'd been seeing.
    https://emacs.stackexchange.com/questions/5461/fix-org-fontification-error
    
    Additional thanks to Sacha:
    https://github.com/sachac/.emacs.d/blob/gh-pages/Sacha.org#about-this-file

--- init.el
+++ init.el
@@ -3,8 +3,17 @@
 ;; This file allows Emacs to initialize my customizations
 ;; in Emacs lisp embedded in *one* literate Org-mode file.
 
+;; This sets up the load path so that we can override it
+(package-initialize nil)
+;; Override the packages with the git version of Org and other packages
+(add-to-list 'load-path "~/.emacs.d/site-lisp/org-mode/lisp/")
+
+;; Load the rest of the packages
+(package-initialize t)
+(setq package-enable-at-startup nil)
+
 (require 'org)
-(org-babel-load-file (concat (getenv "HOME") "/.emacs.d/michael.org"))
+(org-babel-load-file "~/.emacs.d/michael.org")
 
 ;; Keep emacs Custom-settings in separate file
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
