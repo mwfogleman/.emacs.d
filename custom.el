d3da7a5c5d35724959bab9de4fe88c52001b8f26
Michael Fogleman <michaelwfogleman@gmail.com>
2018-10-07 11:04:29 -0700

    Remove solarized stuff from custom.el.

--- custom.el
+++ custom.el
@@ -5,11 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["#262626" "#d70000" "#5f8700" "#af8700" "#0087ff" "#af005f" "#00afaf" "#626262"])
  '(auth-source-save-behavior nil)
- '(background-color nil)
- '(background-mode dark)
  '(cursor-color nil)
- '(custom-enabled-themes (quote (solarized-dark)))
- '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
  '(foreground-color nil))
 
 (custom-set-faces
